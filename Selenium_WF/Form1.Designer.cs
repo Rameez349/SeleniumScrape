f8ebb9a3a83bac19dee6234d9cee018478663da6
Rameez349 <rameez.ahmed349@gmail.com>
2017-08-08 16:32:49 +0500

    uploading project
    uploading project to git

---
+++ Selenium_WF/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Selenium_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Click = new System.Windows.Forms.Button();
+            this.rt_Logs = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WebScraping Application";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "www.CareerBuilder.com";
+            // 
+            // btn_Click
+            // 
+            this.btn_Click.Location = new System.Drawing.Point(89, 213);
+            this.btn_Click.Name = "btn_Click";
+            this.btn_Click.Size = new System.Drawing.Size(90, 23);
+            this.btn_Click.TabIndex = 2;
+            this.btn_Click.Text = "Click To Start";
+            this.btn_Click.UseVisualStyleBackColor = true;
+            this.btn_Click.Click += new System.EventHandler(this.btn_Click_Click);
+            // 
+            // rt_Logs
+            // 
+            this.rt_Logs.Location = new System.Drawing.Point(12, 111);
+            this.rt_Logs.Name = "rt_Logs";
+            this.rt_Logs.Size = new System.Drawing.Size(260, 96);
+            this.rt_Logs.TabIndex = 3;
+            this.rt_Logs.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Real time Logs";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rt_Logs);
+            this.Controls.Add(this.btn_Click);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Click;
+        private System.Windows.Forms.RichTextBox rt_Logs;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Selenium_WF/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace Selenium_WF
+{
+    public partial class Form1 : Form
+    {
+        static IWebDriver driverChrome;
+        IWebElement parent;
+        List<string> JobTitlesList;
+        string[] Aplhabets;
+        string RealtimeLogs;
+
+        public Form1()
+        {
+            InitializeComponent();
+            RealtimeLogs = "Application Launched \n";
+            rt_Logs.Text += RealtimeLogs.ToString();
+            var chromeDriverService = ChromeDriverService.CreateDefaultService(@"C:\chromedriver_win32");
+            chromeDriverService.HideCommandPromptWindow = true;
+            driverChrome =  new ChromeDriver(chromeDriverService, new ChromeOptions());
+           // driverChrome = new ChromeDriver(@"C:\chromedriver_win32");
+            
+            RealtimeLogs = "Initialized Web Driver for Chrome \n";
+            rt_Logs.Text += RealtimeLogs.ToString();
+            RealtimeLogs = "Processing.... \n";
+            rt_Logs.Text += RealtimeLogs.ToString();
+            JobTitlesList = new List<string>();
+            Aplhabets = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            //Aplhabets = new string[] { "C" };
+        }
+
+        private void btn_Click_Click(object sender, EventArgs e)
+        {
+
+            driverChrome.Navigate().GoToUrl("http://www.careerbuilder.com/");
+            BrowserToJobs();
+            RealtimeLogs = "Exporting Listings to Excel File... \n";
+            rt_Logs.Text += RealtimeLogs.ToString();
+            ExportDatatoExcel();
+            RealtimeLogs = "Listings Exported Successfully. \n";
+            rt_Logs.Text += RealtimeLogs.ToString();
+            RealtimeLogs = "Please view 'Listings.csv'\n";
+            rt_Logs.Text += RealtimeLogs.ToString();
+        }
+
+        public void BrowserToJobs()
+        {
+            driverChrome.FindElement(By.Id("header-menu-browse-jobs-link")).SendKeys(OpenQA.Selenium.Keys.Enter);
+
+            for (int i = 0; i < Aplhabets.Length; i++)
+            {
+                JobListingByAlphabet(Aplhabets[i]);
+            }
+        }
+
+        public void JobListingByAlphabet(string Alphabet)
+        {
+            parent = driverChrome.FindElement(By.ClassName("browse-job-titles"));
+            parent.FindElement(By.XPath(".//a[contains(text(),'" + Alphabet + "')]")).Click();
+
+            GetListings();
+        }
+
+        public void GetListings()
+        {
+            var listoftitles = driverChrome.FindElements(By.ClassName("browse-titles-link"));
+            foreach (var title in listoftitles)
+            {
+                var temp_anchor = title.FindElement(By.TagName("a"));
+                JobTitlesList.Add(temp_anchor.Text);
+            }
+        }
+
+        public void ExportDatatoExcel()
+        {
+            if (File.Exists("listings.csv"))
+            {
+                File.Delete("listings.csv");
+            }
+            using (StreamWriter sr = File.CreateText("listings.csv"))
+            {
+                foreach (string listitem in JobTitlesList)
+                {
+                    sr.WriteLine(listitem);
+                }
+            }
+        }
+    }
+}
